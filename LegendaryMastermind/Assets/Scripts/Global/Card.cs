9b534952ca79167447105e604f5ffb986431f9b9
DevenSmith <Smith.Deven.E@gmail.com>
2015-11-16 20:37:46 -0800

    worked on card creation

---
+++ LegendaryMastermind/Assets/Scripts/Global/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+public class Card  
+{
+	public bool MainCard;
+	
+	public string Title;
+	public string SubTitle;
+	
+	public string ImagePath;
+	
+	public int VictoryPoints;
+	public int FightValue;
+	
+	public string FightText;
+	public string AlwaysLeadsText;
+	public string MasterStrikeText;
+	public string SpecialRulesText;
+}
---
+++ LegendaryMastermind/Assets/Scripts/Global/CardContainer.cs
@@ -0,0 +1,38 @@
+﻿// http://wiki.unity3d.com/index.php?title=Saving_and_Loading_Data:_XmlSerializer
+
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+[XmlRoot("CardCollection")]
+public class CardContainer 
+{
+	[XmlArray("Cards")]
+	[XmlArrayItem("Card")]
+	public List<Card> Cards = new List<Card>();
+	
+	public void Save(string path)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(CardContainer));
+		using(FileStream stream = new FileStream(path, FileMode.Create))
+		{
+			serializer.Serialize(stream, this);
+		}
+	}
+	
+	public static CardContainer Load(string path)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(CardContainer));
+		using(FileStream stream = new FileStream(path, FileMode.Open))
+		{
+			return serializer.Deserialize(stream) as CardContainer;
+		}
+	}
+	
+	public static CardContainer LoadFromText(string text)
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(CardContainer));
+		return serializer.Deserialize(new StringReader(text)) as CardContainer;
+	}
+}
